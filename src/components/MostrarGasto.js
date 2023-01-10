d02e5a0ed5c0cbf38cb7139b5a4b7deb96044978
Maxis86 <maximilianochamarro@gmail.com>
2023-01-10 10:12:31 -0300

    cambio de estética en mostrar gastos

--- src/components/MostrarGasto.js
+++ src/components/MostrarGasto.js
@@ -5,6 +5,15 @@ import gastosContext from "../context/gastos/gastosContext";
 import swal from "sweetalert";
 import Swal from 'sweetalert2'
 
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions,
+} from 'react-swipeable-list';
+import 'react-swipeable-list/dist/styles.css';
+
 const MostrarGasto = ({ gasto }) => {
   const gastoContext = useContext(gastosContext);
   const { eliminarGasto, editarGasto, ano } = gastoContext;
@@ -24,7 +33,7 @@ const MostrarGasto = ({ gasto }) => {
       if (result.isConfirmed) {
         Swal.fire(
           'Eliminado!',
-           eliminarGasto(id),
+          eliminarGasto(id),
           'success'
         )
       }
@@ -35,12 +44,12 @@ const MostrarGasto = ({ gasto }) => {
     Swal.fire({
       title: `Datos`,
       html:
-        `<h5>Cargado el ${gasto.dia} de ${gasto.mes} de ${gasto.ano} por ${gasto.usuarioCargado}</h5>` 
+        `<h5>Cargado el ${gasto.dia} de ${gasto.mes} de ${gasto.ano} por ${gasto.usuarioCargado}</h5>`
     })
 
   };
 
-  const editar = async(id, nombre, precio, mes) => {
+  const editar = async (id, nombre, precio, mes) => {
 
     const { value: formValues } = await Swal.fire({
       title: 'Editar Gasto',
@@ -55,58 +64,112 @@ const MostrarGasto = ({ gasto }) => {
         ]
       }
     })
-    
+
     if (formValues) {
       editarGasto(id, formValues[0], formValues[1], mes, ano)
     }
   };
 
+  const leadingActions = () => (
+    <LeadingActions>
+      <SwipeAction onClick={() => editar(gasto._id, gasto.nombre, gasto.precio, gasto.mes)}>
+        Editar
+      </SwipeAction>
+    </LeadingActions>
+  );
+
+  const trailingActions = () => (
+    <TrailingActions>
+      <SwipeAction
+        // destructive={true}
+        onClick={() => eliminar(gasto._id)}
+      >
+        Eliminar 
+      </SwipeAction>
+    </TrailingActions>
+  );
+
 
   return (
     <>
       <div className="row">
-        <div className="col-12">
-          <div className="row">
-            <div className="col-12 d-flex">
-              <h3>
-                <b> {gasto.nombre}</b>
-              </h3>
+        <SwipeableList>
+          <SwipeableListItem
+            leadingActions={leadingActions()}
+            trailingActions={trailingActions()}
+          >
+            <div className="col-12 p-2">
+              <div className="row contenido-gasto">
+                <div className="col-12 d-flex">
+                  <div className="descripcion-nombre">
+                     {gasto.nombre}
+                  </div>
+                </div>
+                <div className="col-3 d-flex justify-content-start">
+                  <button
+                    type="button"
+                    className="btn btn-outline mb-6"
+                    onClick={() => ver(gasto)}
+                  >
+                    <ion-icon color="primary" name="search-sharp"></ion-icon>{" "}
+                  </button>
+                </div>
+                <div className="col-9 d-flex justify-content-end">
+                  <div div className="descripcion-precio">
+                    $ {gasto.precio}
+                  </div>
+                </div>
+                
+              </div>
             </div>
-            <div className="col-12 d-flex justify-content-end">
-              <h3>$ {gasto.precio}</h3>
-            </div>
-          </div>
-        </div>
-        <div className="col-1">
-          <button
-            type="button"
-            className="btn btn-outline m-6"
-            onClick={() => ver(gasto)}
-          >
-            <ion-icon color="primary" name="search-sharp"></ion-icon>{" "}
-          </button>
-        </div>
-        <div className="col-1">
-          <button
-            type="button"
-            className="btn btn-outline m-6"
-            onClick={() => editar(gasto._id, gasto.nombre, gasto.precio, gasto.mes)}
-          >
-            <ion-icon color="warning" name="create-sharp"></ion-icon>{" "}
-          </button>
-        </div>
-        <div className="col-1">
-          <button
-            type="button"
-            className="btn btn-outline m-6"
-            onClick={() => eliminar(gasto._id)}
-          >
-            <ion-icon name="trash"></ion-icon>{" "}
-          </button>
-        </div>
+
+          </SwipeableListItem>
+        </SwipeableList>
       </div>
+
     </>
   );
 };
 
 export default MostrarGasto;
+
+
+// <div className="col-12">
+//               <div className="row">
+//                 <div className="col-12 d-flex">
+//                   <h3>
+//                     <b> {gasto.nombre}</b>
+//                   </h3>
+//                 </div>
+//                 <div className="col-12 d-flex justify-content-end">
+//                   <h3>$ {gasto.precio}</h3>
+//                 </div>
+//               </div>
+//             </div>
+//             <div className="col-1">
+//               <button
+//                 type="button"
+//                 className="btn btn-outline m-6"
+//                 onClick={() => ver(gasto)}
+//               >
+//                 <ion-icon color="primary" name="search-sharp"></ion-icon>{" "}
+//               </button>
+//             </div>
+//             <div className="col-1">
+//               <button
+//                 type="button"
+//                 className="btn btn-outline m-6"
+//                 onClick={() => editar(gasto._id, gasto.nombre, gasto.precio, gasto.mes)}
+//               >
+//                 <ion-icon color="warning" name="create-sharp"></ion-icon>{" "}
+//               </button>
+//             </div>
+//             <div className="col-1">
+//               <button
+//                 type="button"
+//                 className="btn btn-outline m-6"
+//                 onClick={() => eliminar(gasto._id)}
+//               >
+//                 <ion-icon name="trash"></ion-icon>{" "}
+//               </button>
+//             </div>
--- src/config/axios.js
+++ src/config/axios.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 const clienteAxios = axios.create({
     //baseURL : "https://servidor-gastos-compartidos.herokuapp.com/"
     baseURL : "http://localhost:8080"
+    //baseURL : "https://servidor-gastos-compartidos-node-production.up.railway.app/"
+
 });
 
 export default clienteAxios;
